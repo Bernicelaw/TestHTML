0026a051edcfdece3ecdd56b412e63b5efe7c682
Bernicelaw <64132156+Bernicelaw@users.noreply.github.com>
2020-04-29 18:01:05 +0800

    Update List_of_restaurants.php

--- List_of_restaurants.php
+++ List_of_restaurants.php
@@ -19,6 +19,36 @@
                         <td>Menara Standard Chartered, TPM</td>
                         <td>03-22441234</td>
                     </tr>
+                      <?php
+                          $serverName = "samplewebtp046184.database.windows.net";
+                          $connectionOptions = array(
+                                               "Database" => "Restaurant",
+                                               "Uid" => "sampleweb",
+                                               "PWD" => "Sample@web");
+                         //Establishes the connection
+                         $conn = sqlsrv_connect($serverName, $connectionOptions);
+                         if (!$conn)
+                         {
+                             die("Error connection: ".sqlsrv_errors());
+                         }
+                         //Run the SQL command: Select statement (view)
+                         $tsql= "SELECT * FROM [dbo].[restaurant]";
+                         $getResults= sqlsrv_query($conn, $tsql);
+                         if ($getResults == FALSE)
+                         {
+                              die(sqlsrv_errors());
+                         }
+                         while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+                         {
+                               echo "<tr>";
+                               echo "<td>". $row['restaurant_id'] . "</td>";
+                               echo "<td>". $row['restaurant_name'] ."</td>";
+                               echo "<td>". $row['restaurant_address'] . "</td>";
+                               echo "<td>". $row['restaurant_phone'] . "</td>";
+                               echo "</tr>";
+                          }
+                          sqlsrv_free_stmt($getResults);
+                      ?>        
                 </table>
             </center>
        </body>
